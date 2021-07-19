9cc65985eb714b0d184096c6bc9d79a1c0d54ef1
rizkianakbar <rizkianakbar4@gmail.com>
2021-07-19 14:57:26 +0700

    Make a bye query

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { Bye } from "./pages/Bye";
 import { Home } from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
@@ -18,10 +19,14 @@ export const Routes: React.FC = () => {
           <div>
             <Link to="/signup">SignUp</Link>
           </div>
+          <div>
+            <Link to="/bye">Bye</Link>
+          </div>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/bye" component={Bye} />
           </Switch>
         </header>
       </div>
---
+++ src/pages/Bye.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useByeQuery } from "../generated/graphql";
+
+interface Props {}
+
+export const Bye: React.FC<Props> = () => {
+    const {data, loading, error} = useByeQuery()
+
+    if(loading) return <div>Loading...</div>
+    if(error) return <div>Error...</div>
+    if(!data) return <div>No data...</div>
+    
+    return (<div>{data.bye}</div>)
+}
