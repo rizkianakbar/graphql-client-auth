a9db0b99c283b9f29adeaa84d8644675b5737024
fauzanrozzaqo <rizkianakbar4@gmail.com>
2021-07-20 20:03:07 +0700

    ah shit, just forgot to render the page

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { Routes } from "./Routes";
+import {
+  ApolloLink,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { getAccessToken } from "./accessToken";
+import { App } from "./App";
 
 const cache = new InMemoryCache({});
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
-})
+});
 const link = createHttpLink({
   uri: "http://localhost:4000/graphql",
-  credentials: 'include'
+  credentials: "include",
 });
-const authLink = setContext((_, {headers}) => {
-  const accessToken = getAccessToken()
+const authLinkk = setContext((_, { headers }) => {
+  const accessToken = getAccessToken();
   return {
     headers: {
       authorization: accessToken ? `bearer ${accessToken}` : "",
-    }
-  }
-})
+    },
+  };
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  operation.setContext(({ headers }: any) => ({
+    headers: {
+      authorization: accessToken ? `bearer ${accessToken}` : "",
+      ...headers,
+    },
+  }));
+  return forward(operation);
+});
 
 const client = new ApolloClient({
   link: authLink.concat(link),
   // credentials: "include",
-  cache
+  cache,
 });
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Routes />
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );
