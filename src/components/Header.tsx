c16f923b3cf38c29008eade85f522c98af8fac18
fauzanrozzaqo <rizkianakbar4@gmail.com>
2021-07-23 22:55:22 +0700

    Logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useMeQuery } from '../generated/graphql';
+import { setAccessToken } from '../accessToken';
+import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
-  const { data } = useMeQuery({
-    fetchPolicy: 'network-only',
-  });
+  const { data, loading } = useMeQuery();
+  const [logout, { client }] = useLogoutMutation();
+
+  let body: any = null;
+
+  if (loading) {
+    body = null;
+  } else if (data && data.me) {
+    body = <div>you are logged in as: {data.me.email}</div>;
+  } else {
+    body = <div>not logged in</div>;
+  }
+
   return (
     <header>
       <div>
-        <Link to="/">Home</Link>
+        <Link to="/">home</Link>
       </div>
       <div>
-        <Link to="/signin">SignIn</Link>
+        <Link to="/signup">register</Link>
       </div>
       <div>
-        <Link to="/signup">SignUp</Link>
+        <Link to="/signin">login</Link>
       </div>
       <div>
-        <Link to="/bye">Bye</Link>
+        <Link to="/bye">bye</Link>
       </div>
-      {data && data.me ? <div>you are login as: {data.me.email}</div> : null}
+      <div>
+        {!loading && data && data.me ? (
+          <button
+            onClick={async () => {
+              await logout();
+              setAccessToken('');
+              await client!.resetStore();
+            }}
+          >
+            logout
+          </button>
+        ) : null}
+      </div>
+      {body}
     </header>
   );
 };
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { RouteComponentProps } from "react-router-dom";
-import { setAccessToken } from "../accessToken";
-import { useLoginMutation } from "../generated/graphql";
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { setAccessToken } from '../accessToken';
+import { MeDocument, MeQuery, useLoginMutation } from '../generated/graphql';
 
 interface Props {}
 
 export const SignIn: React.FC<RouteComponentProps> = ({ history }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const [login] = useLoginMutation();
 
@@ -15,21 +15,33 @@ export const SignIn: React.FC<RouteComponentProps> = ({ history }) => {
     <form
       onSubmit={async (e) => {
         e.preventDefault();
-        console.log("Form submitted");
+        console.log('Form submitted');
         const response = await login({
           variables: {
             email,
             password,
           },
+          update: (store, { data }) => {
+            if (!data) {
+              return null;
+            }
+            store.writeQuery<MeQuery>({
+              query: MeDocument,
+              data: {
+                __typename: 'Query',
+                me: data.login.user,
+              },
+            });
+          },
         });
 
         console.log(response);
 
-        if(response && response.data) {
-          setAccessToken(response.data.login.accessToken)
+        if (response && response.data) {
+          setAccessToken(response.data.login.accessToken);
         }
 
-        history.push("/");
+        history.push('/');
       }}
     >
       <div>
